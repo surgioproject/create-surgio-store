785a8b688a9d9073c532589498e9cc94793472d9
Roy Li <me@dada.li>
2019-10-28 11:32:41 +0800

    feat: add new demo features

--- template/provider/subscribe_demo.js
+++ template/provider/subscribe_demo.js
@@ -3,12 +3,8 @@
 module.exports = {
   url: 'https://tgbot.lbyczf.com/surge2sswin?url=https://raw.githubusercontent.com/lhie1/Surge/master/Surge.conf',
   type: 'shadowsocks_json_subscribe',
-  udpRelay: true, // 定义所有的节点都支持 udpRelay
-  // 定义哪些节点支持 Netflix，你也可以挑选出几个常用的节点
-  netflixFilter(nodeConfig) {
-    const name = nodeConfig.nodeName.toLowerCase();
-    return [
-      'hk',
-    ].some(key => name.includes(key.toLowerCase()));
-  },
+  // 定义所有的节点都支持 udpRelay
+  udpRelay: true,
+  // 添加国旗 emoji
+  addFlag: true,
 };
